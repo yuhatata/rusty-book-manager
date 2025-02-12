cdef7b60623b9c882878b6ed61ee9e284fb20b35
yuhata <iwazawa@me.com>
2025-02-12 16:25:07 +0900

    chap 3.4.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::net::TcpListener;
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+
+    Ok(axum::serve(listener, app).await?)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
 }
